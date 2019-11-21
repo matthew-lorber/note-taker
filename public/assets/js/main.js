aa69f0e30f58fd6e522d86d98918ff0b92517c3c
Matt Lorber <matthew.lorber@outlook.com>
2019-11-21 13:07:57 -0500

    updated DOM element removal

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,24 +1,26 @@
 window.onload = function() {
-
-    document.getElementById("saveNote").addEventListener("click", function() {
-        
-        var title = document.getElementById("note_title");
-        var body = document.getElementById("note_text");
-        var node = document.createElement("LI");
-        var date = new Date();
-        var now = date.toLocaleString("en-US");
-        var this_id = date.getTime();
-        node.innerHTML = "<li class='a_li' id='" + this_id + "'><span class='bold gray'>" + now + " " + "</span><span class='bold blue'>" + title.value + "</span>: <span>" + body.value + "<span class='delete-button bold blue float-right'>&#11198;</span></span></li>";
-        document.getElementById("note_group").appendChild(node);
-        document.getElementById(this_id).addEventListener("click", delete_li(), false);
-        title.value = "";
-        body.value = "";
+    console.log("loaded");
+    document.addEventListener("click", function(event) {
+        console.log("clickregistered");
+        if (event.target.matches("#saveNote")) {
+            console.log("savenoteclickregistered");
+            var title = document.getElementById("note_title");
+            var body = document.getElementById("note_text");
+            var node = document.createElement("LI");
+            var date = new Date();
+            var now = date.toLocaleString("en-US");
+            var this_id = date.getTime();
+            node.innerHTML = "<li class='a_li' id='" + this_id + "'><span class='bold gray'>" + now + " " + "</span><span class='bold blue'>" + title.value + "</span>: <span>" + body.value + "<span class='delete-button bold blue float-right' id='" + this_id + "'>&#11198;</span></span></li>";
+            document.getElementById("note_group").appendChild(node);
+            title.value = "";
+            body.value = "";
+        } else if (event.target.matches(".delete-button")) {
+            console.log("test", event.target.id);
+            document.getElementById(event.target.id).remove();
+        }
 
     });
 
-    function delete_li() {
-        var this_li = this.id; console.log(this_li)
-        //this_li.remove();
-    }
+  
 
 }
