a1dd2588c155735bdeb73e5634ffd7f0510e7045
Matt Lorber <matthew.lorber@outlook.com>
2019-12-10 01:59:22 -0500

    still working on routing

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
+// Load Express middleware 
 const express = require("express");
 const router = express.Router();
 const connection = require("./db/connection.js");
-// const notes = require("./public/assets/js/notes.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(router);
+
+router.post("/new", function(req, res) {console.log("hit add new note route");
+console.log(req);
+    connection.query("INSERT INTO notes SET ?", [req.body.title], function(err, result) {
+        // rigth here figure out req.body.title and req.body.body PLACE INTO DATABASE
+        if (err) throw err;
+
+        res.json(result);
+    });
+});
 app.use(express.static("public"));
-// app.use(notes);
+
 app.listen(PORT);
 console.log("express server listening on", PORT);
 
 // Export the middleware so other modules can use it
-exports.app = app;
-exports.express = express;
-exports.connection = connection;
-exports.router = router;
+// exports.app = app;
+// exports.express = express;
+// exports.connection = connection;
+// exports.router = router;
 
 //console.log(module.filename); console.log(module.id); console.log(module.exports);
